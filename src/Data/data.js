13dd6cd9fb999e05656ef9e8bb3b58021a211130
khuranamanan <112554344+khuranamanan@users.noreply.github.com>
2023-07-21 21:32:28 +0530

    code completed

---
+++ src/Data/data.js
@@ -0,0 +1,32 @@
+export var PRODUCTS = [
+  {
+    category: "Sporting Goods",
+    price: "$49.99",
+    stocked: true,
+    name: "Football",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$9.99",
+    stocked: true,
+    name: "Baseball",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$29.99",
+    stocked: false,
+    name: "Basketball",
+  },
+  {
+    category: "Electronics",
+    price: "$99.99",
+    stocked: true,
+    name: "iPod Touch",
+  },
+  {
+    category: "Electronics",
+    price: "$399.99",
+    stocked: false,
+    name: "iPhone 5",
+  },
+];
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,54 @@
+import { useState } from "react";
+import { PRODUCTS } from "../Data/data";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  const [checkbox, setCheckBox] = useState([]);
+
+  const categories = PRODUCTS.reduce(
+    (result, { category }) =>
+      result.includes(category) ? result : [...result, category],
+    []
+  );
+
+  function handlecheckboxSelection(event) {
+    if (checkbox.includes(event.target.value)) {
+      setCheckBox(checkbox.filter((cat) => cat !== event.target.value));
+    } else {
+      setCheckBox([...checkbox, event.target.value]);
+    }
+  }
+
+  const filterPro =
+    checkbox.length === 0
+      ? PRODUCTS
+      : PRODUCTS.filter(({ category }) => checkbox.includes(category));
+
+  const productsMapped = filterPro.map((pro, index) => (
+    <div key={index} className="p-4 my-2">
+      <h2>{pro.name}</h2>
+      <p>{pro.price}</p>
+      <p>{pro.category}</p>
+    </div>
+  ));
+
+  return (
+    <div>
+      <div>
+        {categories.map((cat) => (
+          <label key={cat}>
+            <input
+              type="checkbox"
+              value={cat}
+              onChange={handlecheckboxSelection}
+              checked={checkbox.includes(cat)}
+            />{" "}
+            {cat}
+          </label>
+        ))}
+      </div>
+      <div>{productsMapped}</div>
+    </div>
+  );
 }
 
 export default HomePage;
